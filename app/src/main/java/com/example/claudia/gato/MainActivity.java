01aa21546e829a7154ed4d8e4d6ac94c209c3931
Claudia <chamg7@hotmail.com>
2018-05-11 01:58:11 -0500

    Version Final

--- app/src/main/java/com/example/claudia/gato/MainActivity.java
+++ app/src/main/java/com/example/claudia/gato/MainActivity.java
@@ -10,8 +10,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     public Button A1, A2, A3, B1, B2, B3, C1, C2, C3;
-    int turno, contador;
-    EditText J1, J2;
+    public Button again;
+
+    int turno, contador, cont = 0;
+
+    EditText J1, J2, Ganador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +41,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         C1.setOnClickListener(this);
         C2.setOnClickListener(this);
         C3.setOnClickListener(this);
+
+        again = (Button)findViewById(R.id.again);
+        again.setOnClickListener(this);
+
+        J1 = (EditText)findViewById(R.id.nomj1);
+        J2 = (EditText)findViewById(R.id.nomj2);
+        Ganador = (EditText)findViewById(R.id.winname);
+
+
+
     }
 
     @Override
     public void onClick(View view) {
-        contador ++;
-        turno = contador%2;
+        cont ++;
+        turno = cont%2;
 
         if(turno == 1){
             imprimirXO('X', view);
@@ -52,7 +66,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
     public void imprimirXO(char letra, View view){
+
+        if(view.getId() == R.id.again){
+            Ganador.setEnabled(false);
+            Ganador.setText("");
+            J1.setText("");
+            J2.setText("");
+            cont = 0;
+
+            A1.setEnabled(true);
+            A1.setText("");
+            A2.setEnabled(true);
+            A2.setText("");
+            A3.setEnabled(true);
+            A3.setText("");
+            B1.setEnabled(true);
+            B1.setText("");
+            B2.setEnabled(true);
+            B2.setText("");
+            B3.setEnabled(true);
+            B3.setText("");
+            C1.setEnabled(true);
+            C1.setText("");
+            C2.setEnabled(true);
+            C2.setText("");
+            C3.setEnabled(true);
+            C3.setText("");
+
+        }
+
+
         if(view.getId() == R.id.A1){
             A1.setText(letra+"");
             A1.setEnabled(false);
@@ -89,6 +134,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             C3.setText(letra+"");
             C3.setEnabled(false);
         }
+
+        Quien_gano(letra);
+
+    }//FIN DE IMPRIMIR
+
+    public void Quien_gano(char letra){
+        if(A1.getText().toString().equals(letra+"") && A2.getText().toString().equals(letra+"") && A3.getText().toString().equals(letra+"")){
+            Imp_ganador(letra);
+        }else         if(B1.getText().toString().equals(letra+"") && B2.getText().toString().equals(letra+"") && B3.getText().toString().equals(letra+"")){
+            Imp_ganador(letra);
+        }else         if(C1.getText().toString().equals(letra+"") && C2.getText().toString().equals(letra+"") && C3.getText().toString().equals(letra+"")){
+            Imp_ganador(letra);
+        }else         if(A1.getText().toString().equals(letra+"") && B2.getText().toString().equals(letra+"") && C3.getText().toString().equals(letra+"")){
+            Imp_ganador(letra);
+        }else         if(C1.getText().toString().equals(letra+"") && B2.getText().toString().equals(letra+"") && A3.getText().toString().equals(letra+"")){
+            Imp_ganador(letra);
+        }else{
+            Ganador.setText("No Hay, es un Empate");
+            Ganador.setEnabled(false);
+        }
+    }
+
+    public void Imp_ganador(char letra){
+
+        String nom1 = J1.getText().toString();
+        String nom2 = J2.getText().toString();
+
+        if(letra == 'X'){
+            Ganador.setText(nom1);
+        }else{
+            Ganador.setText(nom2);
+        }
+
+        Ganador.setEnabled(false);
+        A1.setEnabled(false);
+        A2.setEnabled(false);
+        A3.setEnabled(false);
+        B1.setEnabled(false);
+        B2.setEnabled(false);
+        B3.setEnabled(false);
+        C1.setEnabled(false);
+        C2.setEnabled(false);
+        C3.setEnabled(false);
     }
 
 }
